e4e5f4d92ce2c16c4f0de0ff5471ce18d97af842
yeiler2111 <yeiler2209@gmail.com>
2025-03-05 19:31:06 -0500

    fix modules

---
+++ payment/src/test/java/edu/unimagdalena/payment/TestPaymentApplication.java
@@ -0,0 +1,11 @@
+package edu.unimagdalena.payment;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PaymentApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
